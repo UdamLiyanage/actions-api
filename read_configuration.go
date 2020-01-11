8947e780786b1ef3da4d43a69c2b26fd87092800
Udam Liyanage <udam1998@gmail.com>
2020-01-11 09:14:42 +0530

    Fix for issue #1

--- read_configuration.go
+++ read_configuration.go
@@ -10,6 +10,7 @@ import (
 func readConfiguration(c *gin.Context) {
 	var config Configuration
 	objID, err := primitive.ObjectIDFromHex(c.Param("id"))
+	checkError(err, c)
 	filter := bson.M{"_id": objID}
 	err = DB.Collection.FindOne(context.TODO(), filter).Decode(&config)
 	checkError(err, c)
